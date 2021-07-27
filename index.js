445566097e53db904e0a95ca2cb6dfe0362b3832
Hae-Ji Park <positive235@gmail.com>
2021-07-26 22:32:22 -0700

    test4

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 const urls = [
     'https://unsplash.com/s/photos/natural',
@@ -48,12 +51,17 @@ var images = [];
     // };
     // xhr.send();
     // // images to localhost:5000/images
+    app.use(express.static(path.join(__dirname, 'public')))
+    app.set('views', path.join(__dirname, 'views'))
+    app.set('view engine', 'ejs')
+    app.get('/', (req, res) => res.send('hello'))
+    
     app.get('/images', (req, res) => {
         //res.send(JSON.stringify({ ...images }));
         res.send(JSON.parse(JSON.stringify(images)));
     });
     const server = app.listen(5000)
-    // console.log('Example app listening at http://localhost:%s', port);    
+    // console.log('Example app listening at %s', port);    
 })();
 
 
