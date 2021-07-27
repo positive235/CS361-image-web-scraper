be10bb3ba1269a5591b6555c4d77786f950eaba2
Hae-Ji Park <positive235@gmail.com>
2021-07-26 21:54:20 -0700

    send images to server

--- image-web-scraper.js
+++ image-web-scraper.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
 const urls = [
     'https://unsplash.com/s/photos/natural',
@@ -13,7 +16,8 @@ const urls = [
     'https://unsplash.com/s/photos/forest',
     'https://unsplash.com/s/photos/flowers',
     'https://unsplash.com/s/photos/grass',
-    'https://unsplash.com/s/photos/flower-garden'
+    'https://unsplash.com/s/photos/flower-garden',
+    'https://unsplash.com/s/photos/vegetation'
 ]
 
 var images = [];
@@ -21,7 +25,7 @@ var images = [];
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
+
     for (var i = 0; i < urls.length; i++) {
         await page.goto(urls[i]);
         const resultsSelector = '.oCCRx';
@@ -29,5 +33,30 @@ var images = [];
         results.forEach(result => images.push(result));    
     }
     await browser.close(); 
-    console.log(images.length, images);
+
+    // Sending a receiving data in JSON format using GET method
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://image-web-scraper-microservice.herokuapp.com/?data=" + encodeURIComponent(JSON.parse(JSON.stringify(images)));
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+        }
+    };
+    xhr.send();
+    // // images to localhost:5000/images
+    // app.get('/images', (req, res) => {
+    //     //res.send(JSON.stringify({ ...images }));
+    //     res.send(JSON.parse(JSON.stringify(images)));
+    // });
+    // const server = app.listen(5000)
+    // console.log('Example app listening at http://localhost:%s', port);    
 })();
+
+
+
+
+
