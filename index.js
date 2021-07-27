6b34eb6e789bc4a1ff4396b19b79c0e034d82ab4
Hae-Ji Park <positive235@gmail.com>
2021-07-26 23:03:09 -0700

    test 6

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
 
 
 
@@ -60,8 +59,12 @@ var images = [];
         //res.send(JSON.stringify({ ...images }));
         res.send(JSON.parse(JSON.stringify(images)));
     });
-    const server = app.listen(port)
-    console.log('Example app listening at %s', port);    
+    var server_port = process.env.PORT || 3000;
+    var server_host = '0.0.0.0';
+    app.listen(server_port, server_host, function() {
+        console.log('Listening on port %d', server_port);
+    })
+    //console.log('Example app listening at %s', port);    
 })();
 
 
